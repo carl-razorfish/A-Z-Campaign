7e2845f75b3bfd72ac68b05526a9caf7a55ae536
davelaser <stuart.thorne@razorfish.com>
2011-04-08 17:54:19 +0100

    Added ios scroll

--- src/js/ui.js
+++ src/js/ui.js
@@ -21,27 +21,5 @@ RIA.UI = {
 		} else {
 			this.navFX.start("top",(this.navOffsetTop+document.body.scrollTop));
 		}
-	},
-	bodyIOSTouchStart: function(e) {
-		this.originalPos = {x:e.targetTouches[0].clientX, y:e.targetTouches[0].clientY};
-		this.startPos = {x:e.targetTouches[0].clientX, y:e.targetTouches[0].clientY};
-		document.body.addEventListener("touchmove", this.bodyIOSTouchMove.bind(this),false);
-	},
-	bodyIOSTouchMove: function(e) {
-		e.preventDefault();
-		var translateY;
-		Log.info("this.shell.offsetTop: "+this.shell.offsetTop);
-		this.currentPos = {x:e.targetTouches[0].clientX, y:e.targetTouches[0].clientY};
-		this.newPos = {
-			x:this.originalPos.x-this.currentPos.x,
-			y:this.originalPos.y-this.currentPos.y
-		}
-		translateY = document.body.style.webkitTransform;
-		translateY = (translateY != "") ? translateY.substring(11) : "0";
-		translateY = translateY.replace(")px","");
-		translateY = parseFloat(translateY);
-		document.body.style.webkitTransition = "all 1s ease-in-out";
-		document.body.style.webkitTransform = "translateY("+(this.newPos.y-translateY)+"px)";
-		this.originalPos = this.currentPos;
 	}
 }
