1a210e79b5d4d041925a1eed5e77b4b4b2386b74
davelaser <stuart.thorne@razorfish.com>
2011-04-19 10:51:50 +0100

    Applied some Opera css3 fixes

--- main.py
+++ main.py
@@ -54,50 +54,47 @@ def getKeyCodes(self):
 		logging.info("NOT Got categorykeycodes from memcache")
 		memcache.add("categorykeycodes", self._keyCodes)
 		return self._keyCodes
-	
-class BaseHandler(webapp.RequestHandler):
-  def get(self):
-	if not hasattr(self, "_current_user"):
-		self._urlPath = "/"
-	return self._urlPath
 
-class HomeHandler(BaseHandler):
+class HomeHandler(webapp.RequestHandler):
   def get(self):
-	timestamp = time.time()
-	atozlistMC = AToZList.load()
-	categoriesMC = categories.load()
-	contentMC = content.load()
-	commonMC = common.load()
-	
-	#mail.send_mail(sender="Razorfish RIA Team London (automated) <razorfish.ria.london@gmail.com>", to="Stuart Thorne <stuart.thorne@razorfish.com>", subject="Test email from GAE", body="""This is a test email from Google App Engine.""")
-
-	keyCodes = getKeyCodes(self)
-	path = os.path.join(os.path.dirname(__file__),'index.html')
-	args = dict(timestamp=timestamp,content=contentMC,common=commonMC,categories=categoriesMC,aToZList=json.dumps(atozlistMC),keyCodes=keyCodes)
-	self.response.out.write(template.render(path,args))
+	try:
+		atozlistMC = AToZList.load()
+		categoriesMC = categories.load()
+		contentMC = content.load()
+		commonMC = common.load()
+		keyCodes = getKeyCodes(self)
+		path = os.path.join(os.path.dirname(__file__),'index.html')
+		args = dict(content=contentMC,common=commonMC,categories=categoriesMC,aToZList=json.dumps(atozlistMC),keyCodes=keyCodes)
+		self.response.out.write(template.render(path,args))
+	except apiproxy_errors.OverQuotaError, message:
+		logging.error(message)
+		mail.send_mail(sender="Razorfish RIA Team London (automated) <razorfish.ria.london@gmail.com>", to="Stuart Thorne <stuart.thorne@razorfish.com>", subject="URGENT : OverQuotaError(HomeHandler) : http://a-z-campaign.appspot.com", body="""The Google App engine web application at http://a-z-campaign.appspot.com has exceeded it's daily Request Quota. Check the Error logs for more details.""")
   def post(self):
     path = os.path.join(os.path.dirname(__file__),'index.html')
     self.response.out.write(template.render(path,{}))
 
 		
-class ViewHandler(BaseHandler):
+class ViewHandler(webapp.RequestHandler):
   def get(self, urlPath):
-	atozlistMC = AToZList.load()
-	categoriesMC = categories.load()
-	contentMC = content.load()
-	commonMC = common.load()
-	timestamp = time.time()
-	alpha = ""
-	category = ""
-	path = os.path.join(os.path.dirname(__file__),'index.html')
-	if urlPath is not None:
-		if len(urlPath) < 2:
-			alpha = urlPath
-		else:
-			category = urlPath
-	keyCodes = getKeyCodes(self)
-	args = dict(timestamp=timestamp,alpha=alpha,category=category,content=contentMC,common=commonMC,categories=categoriesMC,aToZList=json.dumps(atozlistMC),keyCodes=keyCodes)
-	self.response.out.write(template.render(path,args))
+	try:
+		atozlistMC = AToZList.load()
+		categoriesMC = categories.load()
+		contentMC = content.load()
+		commonMC = common.load()
+		alpha = ""
+		category = ""
+		path = os.path.join(os.path.dirname(__file__),'index.html')
+		if urlPath is not None:
+			if len(urlPath) < 2:
+				alpha = urlPath
+			else:
+				category = urlPath
+		keyCodes = getKeyCodes(self)
+		args = dict(alpha=alpha,category=category,content=contentMC,common=commonMC,categories=categoriesMC,aToZList=json.dumps(atozlistMC),keyCodes=keyCodes)
+		self.response.out.write(template.render(path,args))
+	except apiproxy_errors.OverQuotaError, message:
+		logging.error(message)
+		mail.send_mail(sender="Razorfish RIA Team London (automated) <razorfish.ria.london@gmail.com>", to="Stuart Thorne <stuart.thorne@razorfish.com>", subject="URGENT : OverQuotaError(ViewHandler) : http://a-z-campaign.appspot.com", body="""The Google App engine web application at http://a-z-campaign.appspot.com has exceeded it's daily Request Quota. Check the Error logs for more details.""")
   def post(self, urlPath):
     path = os.path.join(os.path.dirname(__file__),'index.html')
     args = dict(urlPath=urlPath)
@@ -105,21 +102,40 @@ class ViewHandler(BaseHandler):
 
 class Error404Handler(webapp.RequestHandler):
   def get(self, urlPath):
-	self.error(404)
-	categoriesMC = categories.load()
-	contentMC = content.load()
-	commonMC = common.load()
-	timestamp = time.time()
-	args = dict(timestamp=timestamp,content=contentMC,common=commonMC,categories=categoriesMC)
-	path = os.path.join(os.path.dirname(__file__),'error.html')
-	self.response.out.write(template.render(path,args))
+	try:
+		self.error(404)
+		categoriesMC = categories.load()
+		contentMC = content.load()
+		commonMC = common.load()
+		timestamp = time.time()
+		args = dict(timestamp=timestamp,content=contentMC,common=commonMC,categories=categoriesMC)
+		path = os.path.join(os.path.dirname(__file__),'error.html')
+		self.response.out.write(template.render(path,args))
+	except apiproxy_errors.OverQuotaError, message:
+		logging.error(message)
+		mail.send_mail(sender="Razorfish RIA Team London (automated) <razorfish.ria.london@gmail.com>", to="Stuart Thorne <stuart.thorne@razorfish.com>", subject="URGENT : OverQuotaError(Error404Handler) : http://a-z-campaign.appspot.com", body="""The Google App engine web application at http://a-z-campaign.appspot.com has exceeded it's daily Request Quota. Check the Error logs for more details.""")
 	
-def main():
+def real_main():
   util.run_wsgi_app(webapp.WSGIApplication([
 	('/',HomeHandler),
 	(regexpURLAtoZ,ViewHandler),
 	(regexpURLError,Error404Handler)
   ]))
 
+def profile_main():
+    # This is the main function for profiling
+    # We've renamed our original main() above to real_main()
+    import cProfile, pstats
+    prof = cProfile.Profile()
+    prof = prof.runctx("real_main()", globals(), locals())
+    print "<pre>"
+    stats = pstats.Stats(prof)
+    stats.sort_stats("time")  # Or cumulative
+    stats.print_stats(80)  # 80 = how many to print
+    # The rest is optional.
+    stats.print_callees()
+    stats.print_callers()
+    print "</pre>"
+
 if __name__ == '__main__':
-  main()
+  real_main()
