679208415b39637f0360e6aaa8994b18624d2f18
davelaser <stuart.thorne@razorfish.com>
2011-05-19 18:51:20 +0100

    Added event tracking for native html5 video player

--- src/js/movie.js
+++ src/js/movie.js
@@ -40,6 +40,54 @@ RIA.Movie = new Class({
 		this.movieContainer = document.id("movie");
 		this.movieContainer.setStyle("display","block");
 		this.movieHTML5 = document.id("movie-html5");
+		
+		if(document.addEventListener) {
+			this.movieHTML5.addEventListener('ended',function() {
+				Log.info("ended native event");
+				this.trackHTML5("Ended");
+			}.bind(this),false);
+		
+			this.movieHTML5.addEventListener('playing',function() {
+				Log.info("playing native event");
+				this.trackHTML5("Playing");
+			}.bind(this),false);
+			
+			this.movieHTML5.addEventListener('pause',function() {
+				Log.info("paused native event");
+				this.trackHTML5("Paused");
+			}.bind(this),false);
+			
+			this.movieHTML5.addEventListener('volumechange',function() {
+				Log.info("volumechange native event");
+				this.trackHTML5("Volume change");
+			}.bind(this),false);
+
+			
+		} else if (document.attachEvent) {
+			this.movieHTML5.attachEvent('ended',function() {
+				Log.info("ended native event");
+				this.trackHTML5("Ended");
+			}.bind(this));
+		
+			this.movieHTML5.attachEvent('playing',function() {
+				Log.info("playing native event");
+				this.trackHTML5("Playing");
+			}.bind(this));
+			
+			this.movieHTML5.attachEvent('pause',function() {
+				Log.info("paused native event");
+				this.trackHTML5("Paused");
+			}.bind(this));
+			
+			this.movieHTML5.attachEvent('volumechange',function() {
+				Log.info("volumechange native event");
+				this.trackHTML5("Volume change");
+			}.bind(this));
+
+		}
+		
+
+		
 		this.movieSWF = document.id("movie-swf");
 		Log.info(this.movieSWF.toElement);
 		this.movieUnsupported = document.id("movie-unsupported");
@@ -82,7 +130,11 @@ RIA.Movie = new Class({
 		this.mask.morph({opacity:"0"});	
 	},
 	trackYT: function(action) {
-		_gaq.push(['_trackEvent', 'YouTubeMovie', action, this.movieSWF.getElement("param[name=movie]").value, null]);
+		_gaq.push(['_trackEvent', 'YouTubeSWFMovie', action, this.movieSWF.getElement("param[name=movie]").value, null]);
+	},
+	trackHTML5: function(action) {
+		Log.info("trackHTML5("+action+")");
+		_gaq.push(['_trackEvent', 'YouTubeHTML5Movie', action, this.movieHTML5.getElements("source")[0].src, null]);
 	},
 	onYouTubePlayerReady: function(playerId) {
 		/*
